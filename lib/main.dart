a0a3b09b80e773d1d9f09472cf24b00c50a008c1
Tanany <itstanany@gmail.com>
2024-04-01 19:19:01 +0200

    refactor on the fly whatsapp connect component to separate dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:odontoo/widgets/OnTheFlyWhatsAppConnect.dart';
+
 
 void main() {
   runApp(MaterialApp(
@@ -17,25 +16,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController controller = TextEditingController();
-  String initialCountry = 'EG';
-  PhoneNumber number = PhoneNumber(isoCode: 'EG');
-  // Replace "yourPhoneNumber" and "yourText" with your values
-  String phone = "";
-  String prefilledMsg = "";
-
-  Future<void> _launchWhatsapp(String phone, String text) async {
-    final url = Uri.parse('whatsapp://send?phone=$phone&text=$text');
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('WhatsApp not installed'),
-        ),
-      );
-    }
-  }
 
 
   @override
@@ -44,62 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text("Odonto Connect"),
       ),
-      body: Row(
-        children: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              // Handle plus button press
-            },
-          ),
-          Expanded(
-            flex: 85, // Occupy 85% of the remaining space
-            child: InternationalPhoneNumberInput(
-              onInputChanged: (PhoneNumber number) {
-                print(number.phoneNumber);
-                phone = number.phoneNumber ?? "";
-              },
-              onInputValidated: (bool value) {
-                print(value);
-              },
-              selectorConfig: SelectorConfig(
-                selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
-                useBottomSheetSafeArea: true,
-              ),
-              ignoreBlank: false,
-              autoValidateMode: AutovalidateMode.disabled,
-              selectorTextStyle: TextStyle(color: Colors.black),
-              initialValue: number,
-              textFieldController: controller,
-              formatInput: true,
-              keyboardType:
-                  TextInputType.numberWithOptions(signed: true, decimal: true),
-              inputBorder: OutlineInputBorder(),
-              onSaved: (PhoneNumber number) {
-                print('On Saved: $number');
-              },
-            ),
-          ),
-          IconButton(
-            icon: const Icon(FontAwesomeIcons.whatsapp),
-            onPressed: () async {
-              // Check if both apps are installed before showing the menu
-              final canLaunchWhatsapp = await canLaunchUrl(
-                  Uri.parse('whatsapp://send?phone=$phone'));
-              if (canLaunchWhatsapp) {
-                _launchWhatsapp(phone, prefilledMsg);
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text(
-                        'Neither WhatsApp nor WhatsApp Business is installed'),
-                  ),
-                );
-              }
-            },
-          ),
-        ],
-      ),
+      body: OnTheFlyWhatsAppConnect()
     );
   }
 }
---
+++ lib/widgets/OnTheFlyWhatsAppConnect.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+class OnTheFlyWhatsAppConnect extends StatefulWidget {
+  const OnTheFlyWhatsAppConnect({super.key});
+
+  @override
+  State<OnTheFlyWhatsAppConnect> createState() => _OnTheFlyWhatsAppConnectState();
+}
+
+class _OnTheFlyWhatsAppConnectState extends State<OnTheFlyWhatsAppConnect> {
+
+  final TextEditingController controller = TextEditingController();
+  String initialCountry = 'EG';
+  PhoneNumber number = PhoneNumber(isoCode: 'EG');
+  // Replace "yourPhoneNumber" and "yourText" with your values
+  String phone = "";
+  String prefilledMsg = "";
+
+  Future<void> _launchWhatsapp(String phone, String text) async {
+    final url = Uri.parse('whatsapp://send?phone=$phone&text=$text');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('WhatsApp not installed'),
+        ),
+      );
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            // Handle plus button press
+          },
+        ),
+        Expanded(
+          flex: 85, // Occupy 85% of the remaining space
+          child: InternationalPhoneNumberInput(
+            onInputChanged: (PhoneNumber number) {
+              print(number.phoneNumber);
+              phone = number.phoneNumber ?? "";
+            },
+            onInputValidated: (bool value) {
+              print(value);
+            },
+            selectorConfig: SelectorConfig(
+              selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+              useBottomSheetSafeArea: true,
+            ),
+            ignoreBlank: false,
+            autoValidateMode: AutovalidateMode.disabled,
+            selectorTextStyle: TextStyle(color: Colors.black),
+            initialValue: number,
+            textFieldController: controller,
+            formatInput: true,
+            keyboardType:
+            TextInputType.numberWithOptions(signed: true, decimal: true),
+            inputBorder: OutlineInputBorder(),
+            onSaved: (PhoneNumber number) {
+              print('On Saved: $number');
+            },
+          ),
+        ),
+        IconButton(
+          icon: const Icon(FontAwesomeIcons.whatsapp),
+          onPressed: () async {
+            // Check if both apps are installed before showing the menu
+            final canLaunchWhatsapp = await canLaunchUrl(
+                Uri.parse('whatsapp://send?phone=$phone'));
+            if (canLaunchWhatsapp) {
+              _launchWhatsapp(phone, prefilledMsg);
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text(
+                      'Neither WhatsApp nor WhatsApp Business is installed'),
+                ),
+              );
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
