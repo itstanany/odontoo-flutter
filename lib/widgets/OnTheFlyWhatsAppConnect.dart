593445d0560dcc530cd516160a1852c1b8bfca33
Tanany <itstanany@gmail.com>
2024-04-02 13:15:56 +0200

    test saving new contact

--- lib/widgets/OnTheFlyWhatsAppConnect.dart
+++ lib/widgets/OnTheFlyWhatsAppConnect.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:odontoo/data/db/entities/UserContactDBEntity.dart';
+import 'package:odontoo/data/db/user_database.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 
@@ -19,6 +21,12 @@ class _OnTheFlyWhatsAppConnectState extends State<OnTheFlyWhatsAppConnect> {
   // Replace "yourPhoneNumber" and "yourText" with your values
   String phone = "";
   String prefilledMsg = "";
+
+
+  final db = $FloorUserDatabase
+      .databaseBuilder("user_database.db")
+      .build();
+
 
   Future<void> _launchWhatsapp(String phone, String text) async {
     final url = Uri.parse('whatsapp://send?phone=$phone&text=$text');
@@ -42,6 +50,12 @@ class _OnTheFlyWhatsAppConnectState extends State<OnTheFlyWhatsAppConnect> {
           icon: Icon(Icons.add),
           onPressed: () {
             // Handle plus button press
+           db.then((value) =>
+           value
+               .contactDao
+               .insertContact(UserContactDBEntity(id: "fdsfdsf", name: "Tanany", countryCodes: ["+20"], numbers: ["01017001982"], note: "First Contact Added"))
+               .then((value) => print(value))
+           );
           },
         ),
         Expanded(
