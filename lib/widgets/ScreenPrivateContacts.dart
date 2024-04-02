b6942f95ad17eb974095757ef7154cfd4692ba1b
Tanany <itstanany@gmail.com>
2024-04-02 16:07:25 +0200

    successful ui communication with the database

--- lib/widgets/ScreenPrivateContacts.dart
+++ lib/widgets/ScreenPrivateContacts.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data/db/user_database.dart';
+
 class ScreenPrivateContact extends StatefulWidget {
   const ScreenPrivateContact({super.key});
 
@@ -8,8 +10,23 @@ class ScreenPrivateContact extends StatefulWidget {
 }
 
 class _ScreenPrivateContactState extends State<ScreenPrivateContact> {
+  String contacts = "Placeholder... Loading";
+  final db = $FloorUserDatabase.databaseBuilder("user_database.db").build();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    db.then((value) =>
+        value.contactDao.getAllContacts()
+            .then((value) => setState(() {
+              contacts = value.first.name.toString();
+            })
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Private Contact Screen");
+    return Text(contacts);
   }
 }
