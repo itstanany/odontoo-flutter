f3030ef1704b791cf663423db82326fb77e0cbe8
Tanany <itstanany@gmail.com>
2024-04-03 18:06:20 +0200

    build list to render contacts

--- lib/widgets/ScreenPrivateContacts.dart
+++ lib/widgets/ScreenPrivateContacts.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:odontoo/data/db/entities/UserContactDBEntity.dart';
 
 import '../data/db/user_database.dart';
 
@@ -10,7 +12,7 @@ class ScreenPrivateContact extends StatefulWidget {
 }
 
 class _ScreenPrivateContactState extends State<ScreenPrivateContact> {
-  String contacts = "Placeholder... Loading";
+  List<UserContactDBEntity> _contacts = [];
   final db = $FloorUserDatabase.databaseBuilder("user_database.db").build();
 
   @override
@@ -20,13 +22,55 @@ class _ScreenPrivateContactState extends State<ScreenPrivateContact> {
     db.then((value) =>
         value.contactDao.getAllContacts()
             .then((value) => setState(() {
-              contacts = value.first.name.toString();
+              _contacts = value;
             })
         ));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Text(contacts);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Odonto Connect"),
+      ),
+      body: ListView.builder(
+        itemCount: _contacts.length,
+        itemBuilder: (context, index) {
+          final contact = _contacts[index];
+          return ListTile(
+            title: Text(contact.name),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min, // Control trailing icon size
+              children: [
+                IconButton(
+                  icon: const Icon(FontAwesomeIcons.whatsapp),
+                  onPressed: () async {
+                    // Check if both apps are installed before showing the menu
+                    // final canLaunchWhatsapp = await canLaunchUrl(
+                    //     Uri.parse('whatsapp://send?phone=$phone'));
+                    // if (canLaunchWhatsapp) {
+                    //   _launchWhatsapp(phone, prefilledMsg);
+                    // } else {
+                    //   ScaffoldMessenger.of(context).showSnackBar(
+                    //     const SnackBar(
+                    //       content: Text(
+                    //           'Neither WhatsApp nor WhatsApp Business is installed'),
+                    //     ),
+                    //   );
+                    // }
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.call),
+                  onPressed: () {
+                    // Handle phone call action (launch dialer)
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
