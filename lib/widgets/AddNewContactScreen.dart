b1e122eeea336e06c4e06bfe4a4cd4d40b220186
Tanany <itstanany@gmail.com>
2024-04-04 19:16:31 +0200

    populate add new user contact screen

--- lib/widgets/AddNewContactScreen.dart
+++ lib/widgets/AddNewContactScreen.dart
@@ -1,5 +1,6 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 class AddNewContactScreen extends StatefulWidget {
   const AddNewContactScreen({super.key});
@@ -9,8 +10,61 @@ class AddNewContactScreen extends StatefulWidget {
 }
 
 class _AddNewContactScreenState extends State<AddNewContactScreen> {
+  final TextEditingController controller = TextEditingController();
+  PhoneNumber number = PhoneNumber(isoCode: 'EG');
+
+  // Replace "yourPhoneNumber" and "yourText" with your values
+  String phone = "";
+  bool showNoteInputField = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Add New Contact Screen Placeholder");
+    return Column(
+      children: [
+        TextField(
+          autocorrect: false,
+          decoration:
+              InputDecoration(border: OutlineInputBorder(), hintText: "الاسم"),
+        ),
+        InternationalPhoneNumberInput(
+          onInputChanged: (PhoneNumber number) {
+            print(number.phoneNumber);
+            phone = number.phoneNumber ?? "";
+          },
+          onInputValidated: (bool value) {
+            print(value);
+          },
+          selectorConfig: SelectorConfig(
+            selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+            useBottomSheetSafeArea: true,
+          ),
+          ignoreBlank: false,
+          autoValidateMode: AutovalidateMode.onUserInteraction,
+          selectorTextStyle: TextStyle(color: Colors.black),
+          initialValue: number,
+          textFieldController: controller,
+          formatInput: true,
+          keyboardType:
+              TextInputType.numberWithOptions(signed: true, decimal: true),
+          inputBorder: OutlineInputBorder(),
+          onSaved: (PhoneNumber number) {
+            print('On Saved: $number');
+          },
+        ),
+        OutlinedButton(onPressed: () {
+          setState(() {
+            showNoteInputField = true;
+          });
+        }, child: Text("Press Here to add Notes -- optional")
+        ),
+        if(showNoteInputField) TextField(
+          autocorrect: false,
+          decoration: InputDecoration(
+            hintText: "Notes",
+            border: OutlineInputBorder(),
+          ),
+        )
+      ],
+    );
   }
 }
