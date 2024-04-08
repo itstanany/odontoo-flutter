ea42350bb9a24df1c9fa56829a767125d1a66d1a
Tanany <itstanany@gmail.com>
2024-04-08 15:37:00 +0200

    use pattern matching in switch statement

--- lib/presentation/add_user_contact_screen/bloc/add_user_contact_bloc.dart
+++ lib/presentation/add_user_contact_screen/bloc/add_user_contact_bloc.dart
@@ -10,22 +10,23 @@ class AddUserContactBloc
     on<AddUserContactEvent>((event, emit) {
       // TODO: implement event handler
       switch (event) {
-        case _SubmitBtnClicked():
-        // TODO: Handle this case.
-        case _AddUserPhoneNumClicked():
-        // TODO: Handle this case.
-        case PhoneNumChanged():
-          if (event is PhoneNumChanged) {
-            print(
-                "handling phone num changed, ${event.index}-${event.countryPhoneCode}-${event.phoneNumber}");
-            final updatedFormInput = state.addUserContactFormInput.copyWith(
-              numbers: [...state.addUserContactFormInput.numbers]
-                ..[event.index] = event.phoneNumber,
-              dialCodes: [...state.addUserContactFormInput.dialCodes]
-                ..[event.index] = event.countryPhoneCode,
-            );
-            emit(AddUserContactStateActive(updatedFormInput));
-          }
+        case _SubmitBtnClicked sbc:
+          // TODO: Handle this case.
+          break;
+        case _AddUserPhoneNumClicked aupnc:
+          // TODO: Handle this case.
+          break;
+        case PhoneNumChanged pnc:
+          print(
+              "handling phone num changed, ${pnc.index}-${event.countryPhoneCode}-${event.phoneNumber}");
+          final updatedFormInput = state.addUserContactFormInput.copyWith(
+            numbers: [...state.addUserContactFormInput.numbers]..[event.index] =
+                event.phoneNumber,
+            dialCodes: [...state.addUserContactFormInput.dialCodes]
+              ..[event.index] = event.countryPhoneCode,
+          );
+          emit(AddUserContactStateActive(updatedFormInput));
+          break;
       }
     });
   }
